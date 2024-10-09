df706bfb478ed99302889eaa883fa09d7e6c528b
Adalberto15F <adalbertosays15@gmail.com>
2024-10-09 09:57:46 -0400

    criando Rota controller Nutrition

--- src/controllers/CreateNutritionController.ts
+++ src/controllers/CreateNutritionController.ts
@@ -1,10 +1,15 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { CreateNutritionService } from '../services/CreateNutritionService'
 
 class CreateNutritionController {
   async handle(request: FastifyRequest, reply: FastifyReply){
     console.log("Rota foi chamada!!")
 
-    reply.send({ message: "Rota foi chamada meu cumpadi!"})
+    const createNutrition = new CreateNutritionService();
+
+    const nutrition = await createNutrition.execute();
+
+    reply.send(nutrition);
   }
 }
 
---
+++ src/services/CreateNutritionService.ts
@@ -0,0 +1,10 @@
+
+class CreateNutritionService {
+  async execute() {
+    console.log("Teste")
+
+    return { message: "Funcionou o service meu cumpadi"}
+  }
+}
+
+export { CreateNutritionService }
